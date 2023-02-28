bfec2d3730ae7e67abca5eddf405c0199367cf6f
Tusharlodhi <123438144+Tusharlodhi@users.noreply.github.com>
2023-02-28 16:45:04 +0530

    Add files via upload

---
+++ room1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int a,b;
+	
+	printf("enter the value of a : ");
+	scanf("%d",&a);
+	
+	printf("enter the value of b : ");
+	scanf("%d",&b);
+	
+	printf("addition is : %d  and subtraction is : %d" ,a+b,a-b);
+	
+	return 0;
+	
+	
+	
+}
